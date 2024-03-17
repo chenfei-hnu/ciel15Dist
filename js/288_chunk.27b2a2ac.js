0bb3562b6c2142619406a3a52714cf7a5cf776af
刘磊 <lei.liu@goland.cn>
2024-03-17 20:12:10 +0800

    1

---
+++ js/288_chunk.27b2a2ac.js
@@ -0,0 +1,83 @@
+"use strict";
+(self["webpackChunkreact_cli"] = self["webpackChunkreact_cli"] || []).push([[288],{
+
+/***/ 4288:
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+// ESM COMPAT FLAG
+__webpack_require__.r(__webpack_exports__);
+
+// EXPORTS
+__webpack_require__.d(__webpack_exports__, {
+  "default": () => (/* binding */ pages_CallForPapers)
+});
+
+// EXTERNAL MODULE: ./node_modules/react/index.js
+var react = __webpack_require__(6540);
+;// CONCATENATED MODULE: ./src/pages/CallForPapers/index.less
+// extracted by mini-css-extract-plugin
+/* harmony default export */ const CallForPapers = ({"researchDirectionWrap":"index_researchDirectionWrap_D1T6ZM","title":"index_title__CJeYK","content":"index_content_Lxrg7e","clear":"index_clear_LcOSLD","mainTitle":"index_mainTitle_oTM5DL","listTitle":"index_listTitle_nSFfkw"});
+// EXTERNAL MODULE: ./node_modules/react-redux/es/index.js + 20 modules
+var es = __webpack_require__(1225);
+;// CONCATENATED MODULE: ./src/pages/CallForPapers/index.tsx
+
+
+
+function CallForPapers_CallForPapers() {
+    return (react.createElement("div", { className: CallForPapers.researchDirectionWrap },
+        react.createElement("div", { className: CallForPapers.title }, "Call for Papers - \u5F81\u7A3F"),
+        react.createElement("div", { className: CallForPapers.content },
+            react.createElement("p", { className: CallForPapers.mainTitle }, "\u7B2C\u5341\u4E94\u5C4A\u6F14\u5316\u8BED\u8A00\u5B66\u56FD\u9645\u7814\u8BA8\u4F1A (CIEL-15)"),
+            react.createElement("p", { className: CallForPapers.mainTitle }, "\u4E00\u53F7\u901A\u77E5"),
+            react.createElement("p", { className: CallForPapers.mainTitle }, "The 15th International Conference in Evolutionary Linguistics"),
+            react.createElement("p", { className: CallForPapers.mainTitle }, "(Notice No.1 of CIEL-15)"),
+            react.createElement("div", { className: CallForPapers.clear }),
+            react.createElement("p", null, "\u7B2C\u5341\u4E94\u5C4A\u6F14\u5316\u8BED\u8A00\u5B66\u56FD\u9645\u7814\u8BA8\u4F1A\uFF08CIEL-15\uFF09\u5C06\u4E8E2024\u5E747\u670812\u65E5\u81F314\u65E5\u5728\u6E56\u5357\u5927\u5B66\u4E3E\u884C\uFF0C\u7531\u6E56\u5357\u5927\u5B66\u5916\u56FD\u8BED\u5B66\u9662\u4E3B\u529E\uFF0C\u6DF1\u5733\u5E02\u795E\u7ECF\u79D1\u5B66\u7814\u7A76\u9662\u534F\u529E\u3002"),
+            react.createElement("p", null, "The 15th International Conference in Evolutionary Linguistics (CIEL-15) will be held during July 12-14, 2024 at Hunan University. CIEL-15 will be held by School of Foreign Languages, Hunan University, and co-organized by the Shenzhen Institute of Neuroscience."),
+            react.createElement("p", null, "\u4F1A\u8BAE\u5C06\u9080\u8BF7\u56FD\u5185\u5916\u76F8\u5173\u9886\u57DF\u6709\u4EE3\u8868\u6027\u548C\u5F71\u54CD\u529B\u7684\u5B66\u8005\u505A\u5927\u4F1A\u7684\u4E3B\u9898\u62A5\u544A\u3002\u4ECA\u606D\u9080\u5404\u4F4D\u5B66\u8005\u540C\u9053\u62D4\u5197\u53C2\u4F1A\uFF0C\u5E76\u656C\u8BF7\u4E8E2024\u5E745\u67081\u65E5\u524D\u5C06\u53C2\u4F1A\u8BBA\u6587\u9898\u76EE\u53CA\u6458\u8981\uFF08\u7EA6400\u2014800\u5B57\uFF09\u4EE5word\u683C\u5F0F\u53D1\u9001\u81F3ciel15hnu@gmail.com\u3002\u4F1A\u8BAE\u6B63\u5F0F\u9080\u8BF7\u51FD\u5C06\u4E8E2024\u5E745\u670831\u65E5\u524D\u53D1\u51FA\u3002\u7814\u8BA8\u4F1A\u5B66\u672F\u8BED\u8A00\u4E3A\u6C49\u8BED\u666E\u901A\u8BDD\u548C\u82F1\u8BED\u3002"),
+            react.createElement("p", null, "CIEL-15 will invite scholars of representative and influential figures in the relevant fields from both domestic and international arenas to deliver keynote speeches. We cordially invite all scholars to attend, and kindly request that the titles and abstracts (approximately 400-800 words) of the papers to be presented at the conference be submitted in Word format to ciel15hnu@gmail.com by May 1, 2024. The official invitation letter will be issued before May 31, 2024. The working languages of CIEL-15 are Mandarin and English."),
+            react.createElement("div", { className: CallForPapers.listTitle }, "\u4F1A\u8BAE\u8BAE\u9898"),
+            react.createElement("ul", null,
+                react.createElement("li", null, "\uFF081\uFF09\u6C49\u8BED\u53CA\u5176\u4ED6\u8BED\u8A00\u7684\u7956\u5148\uFF1B"),
+                react.createElement("li", null, "\uFF082\uFF09\u5178\u578B\u548C\u975E\u5178\u578B\u4EBA\u7FA4\u7684\u8BED\u8A00\u53D1\u5C55;"),
+                react.createElement("li", null, "\uFF083\uFF09\u8001\u9F84\u5316\u4E0E\u8BED\u8A00\u9000\u5316\uFF1B"),
+                react.createElement("li", null, "\uFF084\uFF09\u97F3\u4E50\u4E0E\u8BED\u8A00\u6F14\u5316;"),
+                react.createElement("li", null, "\uFF085\uFF09\u8BED\u8A00\u3001\u8BA4\u77E5\u4E0E\u5927\u8111\uFF1B"),
+                react.createElement("li", null, "\uFF086\uFF09\u65B9\u8A00\u7684\u5F62\u6210\u4E0E\u6F14\u5316\uFF1B"),
+                react.createElement("li", null, "\uFF087\uFF09\u8BED\u8A00\u7684\u5171\u65F6\u4E0E\u5386\u65F6\u7814\u7A76\uFF1B"),
+                react.createElement("li", null, "\uFF088\uFF09\u5927\u578B\u8BED\u8A00\u6A21\u578B\u7684\u53D1\u5C55\u4E0E\u6F14\u5316\uFF1B"),
+                react.createElement("li", null, "\uFF089\uFF09\u5176\u4ED6\u8BED\u8A00\u76F8\u5173\u7684\u8BAE\u9898")),
+            react.createElement("div", { className: CallForPapers.listTitle }, "Conference topics may include the following:"),
+            react.createElement("ul", null,
+                react.createElement("li", null, "1.\tThe proto-language of Chinese and other languages;"),
+                react.createElement("li", null, "2.\tLanguage development in typical and atypical populations;"),
+                react.createElement("li", null, "3.\tAgeing and language degradation;"),
+                react.createElement("li", null, "4.\tThe (co)evolution of language and music;"),
+                react.createElement("li", null, "5.\tLanguage, cognition, and the brain;"),
+                react.createElement("li", null, "6.\tDialect formation and evolution;"),
+                react.createElement("li", null, "7.\tDiachronic and synchronic study of language;"),
+                react.createElement("li", null, "8.\tEvolution of large language model;"),
+                react.createElement("li", null, "9.\tOther language-related research.")),
+            react.createElement("div", { className: CallForPapers.listTitle }, "\u4F1A\u8BAE\u7EC4\u7EC7"),
+            react.createElement("ul", null,
+                react.createElement("li", null, "\uFF081\uFF09\u7814\u8BA8\u4F1A\u540D\u8A89\u4E3B\u5E2D\uFF1A\u738B\u58EB\u5143"),
+                react.createElement("li", null, "\uFF082\uFF09\u4E3B\u5E2D\u56E2\uFF1A\u9648\u98DE\u3001\u5F6D\u521A\u3001\u8C2D\u529B\u6D77"),
+                react.createElement("li", null, "\uFF083\uFF09\u7535\u5B50\u90AE\u7BB1\uFF1Aciel15hnu@gmail.com"),
+                react.createElement("li", null, "\uFF084\uFF09\u4F1A\u8BAE\u4E3B\u9875\uFF1Ahttp://www.ciel-15.com")),
+            react.createElement("div", { className: CallForPapers.listTitle }, "Conference Organization:"),
+            react.createElement("ul", null,
+                react.createElement("li", null, "1.\tHonorary Chair: WANG S-Y.William"),
+                react.createElement("li", null, "2.\tOrganizing Committee: CHEN Fei, PENG Gang, TAN Lihai"),
+                react.createElement("li", null, "3.\tEmail: ciel15hnu@gmail.com"),
+                react.createElement("li", null, "4.\tConference Website: http://www.ciel-15.com")))));
+}
+var mapStateToProps = function (_a) {
+    var common = _a.common;
+    return ({ common: common });
+};
+/* harmony default export */ const pages_CallForPapers = ((0,es/* connect */.Ng)(mapStateToProps)(CallForPapers_CallForPapers));
+
+
+/***/ })
+
+}]);
